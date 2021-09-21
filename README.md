c6e9165b31bd94751a414139b498e30e3c02f45b
Phillip_Gen <phillip.kinney@generation.org>
2021-09-21 18:24:40 -0400

    readme updates

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 2. A `Generate Random Names` button that initiates the selection of 4 random names.
 3. The selected random names are displayed to the browser. 
 4. Each time the `Generate Random Names` button is pressed, a new set of names appears (replacing the previous set of 4 random names)
-4. A button named `clear`  that removes the populated list from the screen
+4. A button named `Clear`  that removes the populated list from the screen
 4. The application should be designed to be visually pleasing. Note: Do not spend too much time on design. Focus primarily on functionality(i.e. steps 1-5).
 
 
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+// Your application should meet the requirements listed below:
+// 1. Selects from a pool of at least ten names of your classmates.
+// 2. A 'Generate Random Names' button that initiates the selection of 4 random names.
+// 3. The selected random names are displayed to the browser.
+// 4. Each time the Generate Random Names button is pressed, a new set of names appears 
+//    (replacing the previous set of 4 random names)
+// 5. A button named 'Clear' that removes the populated list from the screen
+// 6. The application should be designed to be visually pleasing. 
+// Note: Do not spend too much time on design. Focus primarily on functionality(i.e. steps 1-5).
