db2145f2ad0d973479f26b845c168261f8c16870
Willis Feliz <wfeliz1@binghamton.edu>
2021-09-22 13:35:40 -0700

    I have made an edit to a file!

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,24 @@
 // 5. A button named 'Clear' that removes the populated list from the screen
 // 6. The application should be designed to be visually pleasing. 
 // Note: Do not spend too much time on design. Focus primarily on functionality(i.e. steps 1-5).
+
+const names = [‘Kayla’, ‘Willis’, ‘Henok’, ‘Jon’, ‘Alexandra’];
+// element = document.getElementById(“name_list”);
+const randomButtonEl = document.getElementById(“random_button”);
+const clearButtonEl = document.getElementById(“clear_list”);
+function generateRandomNames () {
+    clearList()
+    for (let i = 1; i < 5; i++) {
+          let randomName = names[Math.floor(Math.random()*names.length)];
+          let  paragraph= document.createElement(‘li’);
+        paragraph.id = ‘info’;
+        paragraph.innerHTML = randomName
+          document.getElementById(‘name_list’).appendChild(paragraph);
+    }
+}
+function clearList()
+{
+    document.getElementById(‘name_list’).innerHTML = “”;
+}
+randomButtonEl.onclick = generateRandomNames;
+clearButtonEl.onclick = clearList;
